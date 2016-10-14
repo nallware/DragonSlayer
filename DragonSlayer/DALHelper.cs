53d16b4dcc6f5bb71963cb2364616f85850fa81e
KCCCURB\knall <knall@kcccurbs.com>
2016-10-14 13:46:18 -0400

    Initial commit

---
+++ DragonSlayer/DALHelper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DragonSlayer
+{
+    public static class DALHelper
+    {
+        public static DataTable Query2DataTable(string dataQuery, string sqlConString)
+        {
+            DataTable dt = new DataTable();
+            using (SqlDataAdapter sd = new SqlDataAdapter(dataQuery, sqlConString))
+            {                
+                sd.Fill(dt);
+            }
+            return dt;
+        }
+
+
+        public static List<string> Query2StringList(string listQuery, string sqlConString)
+        {
+            List<string> retlist = new List<string>();
+
+            using (SqlConnection sqlCon = new SqlConnection(sqlConString))
+            {
+                using (SqlCommand cmd = new SqlCommand(listQuery, sqlCon))
+                {
+                    sqlCon.Open();
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            retlist.Add(reader.GetString(0));
+                        }
+                    }
+                }
+            }
+            return retlist;
+        }
+        
+
+    }
+
+}
---
+++ DragonSlayer/FileHelper.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+
+namespace DragonSlayer
+{
+    public static class FileHelper
+    {
+        public static string UniqueFileName(string path, string filename, string extension)
+        {
+            string oldname = filename;
+            if (File.Exists(path + "\\" + filename + extension))
+            {
+                int i = 1;
+                while (File.Exists(path + "\\" + filename + extension))
+                {
+                    filename = filename.Replace(filename, oldname + "(" + i + ")");
+                    i++;
+                }
+
+            }
+            return filename + extension;
+        }
+    }
+}
